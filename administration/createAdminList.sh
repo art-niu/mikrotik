5e252461a617d50724ae362af8229f3abb60e580
Arthur Niu <arthurniu@The-Flower-Of-Winter.local>
2022-01-02 23:17:15 -0500

    add administration scripts.

---
+++ administration/createAdminList.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+  echo "Syntax: $0 <admin ip list>"
+  exit $LINENO
+fi
+
+adminIpListFile=$1
+if [ ! -f $adminIpListFile ]; then
+  echo "Cannot find file $adminIpListFile"
+  exit $LINENO
+fi
+
+cmdLine="/ip firewall address-list "
+
+for line in $(grep -v "^#" $adminIpListFile)
+do
+  cmdLine="$cmdLine 
+add list=admin_ips address=$line"
+done
+
+echo "$cmdLine"
