ed9604b2357d12b305702d7c9757b039ac59ff79
art-niu <arthur.niu@gmail.com>
2022-04-04 22:38:00 -0400

    add input format 00:33 (hh:mm)

--- operation/extendInternetHour.sh
+++ operation/extendInternetHour.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # set -x
 if [ $# -lt 2 ]; then
-	echo "Syntax: $0 <machine name> <extend time>"
+	echo "Syntax: $0 <device tag> <extend minutes | specified time>"
+	echo "$0 HOME 270"
+	echo "$0 GAME 23:30"
 	exit $LINENO
 fi
 
@@ -19,32 +21,27 @@ if [ ! -z "$ROUTERPUBLICKEY" ]; then
 fi
 
 machineName=$1
-extendedMinutes=$2
+extendedTo=$2
 
 startTime="6h00m"
 echo Schedule for $machineName
 dailySchedule="6h00m-22h30m"
-#if [[ $machineName =~ PN* ]]; then
-#	dailySchedule="${startTime}-07h30m"
-#fi
 kControlNumber=4
 weekDays="sun mon tue wed thu fri sat"
-#weekDays=`date +%a |tr [A-Z] [a-z]`
-
-for w in $weekDays
-do
-  cmdLine="/ip kid-control set ${w}=\"${dailySchedule}\"  [/ip k find where name ~ \"${machineName}\"]"
-  echo ${cmdLine}
-  ssh $options $ROUTERUSER@$ROUTERIP ${cmdLine}
-done
 
 currentHour=`date +%H`
 currentMinutes=`date +%M`
-toMinutes=`expr $currentMinutes + $extendedMinutes`
-overHour=`expr $toMinutes / 60`
-remainMinutes=`expr $toMinutes % 60`
-toHour=`expr $currentHour + $overHour`
 
+timeFormat="([0-9]*):([0-9]*)"
+if [[ $extendedTo =~ $timeFormat ]]; then
+  toHour=${BASH_REMATCH[1]}
+  remainMinutes=${BASH_REMATCH[2]}
+else
+  toMinutes=`expr $currentMinutes + $extendedTo`
+  overHour=`expr $toMinutes / 60`
+  remainMinutes=`expr $toMinutes % 60`
+  toHour=`expr $currentHour + $overHour`
+fi
 if [ $toHour -gt 23 ]; then
         toHour=23
         remainMinutes=59
@@ -53,16 +50,22 @@ fi
 day=`date +%a`
 formatedDay=`echo $day|tr [A-Z] [a-z]`
 
-cmdLine="/ip kid-control set ${formatedDay}=\"${startTime}-${toHour}h${remainMinutes}m\" [/ip k find where name ~ \"${machineName}\"]"
+cmdLine="/ip kid-control set ${formatedDay}=\"${currentHour}h${currentMinutes}m-${toHour}h${remainMinutes}m\" [/ip k find where name ~ \"${machineName}\"]"
 
 echo $cmdLine
-
-# "/ip kid-control set ="7h00m-23h00m" [/ip k find where name ~ "PN - Desktop"]
 
 # echo "/ip firewall filter set time=${currentHour}h${currentMinutes}m-${toHour}h${remainMinutes}m,${formatedDay} [/ip firewall filter find where comment~\"Allow Discord \"]"
 
 ssh $options $ROUTERUSER@$ROUTERIP ${cmdLine}
 
+weekDays=`echo $weekDays | sed -e s/${formatedDay}//g`
+
+for w in $weekDays
+do
+  cmdLine="/ip kid-control set ${w}=\"${dailySchedule}\"  [/ip k find where name ~ \"${machineName}\"]"
+  echo ${cmdLine}
+  ssh $options $ROUTERUSER@$ROUTERIP ${cmdLine}
+done
 
 ssh $options $ROUTERUSER@$ROUTERIP "/ip k print"
 
